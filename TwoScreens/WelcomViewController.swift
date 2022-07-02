13098642e7e192c021550244b2c76660aafc949e
Oksenoyt <a.oksenoyt@gmail.com>
2022-07-02 07:26:48 +0300

    2 views with interface elements

---
+++ TwoScreens/WelcomViewController.swift
@@ -0,0 +1,16 @@
+//
+//  WelcomViewController.swift
+//  TwoScreens
+//
+//  Created by Elena Kholodilina on 02.07.2022.
+//
+
+import UIKit
+
+class WelcomViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
