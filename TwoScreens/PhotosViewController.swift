6a7caca3a046af2e867c8c57cdee839ac8773e97
Oksenoyt <a.oksenoyt@gmail.com>
2022-07-06 08:28:01 +0300

    Before folder creation

---
+++ TwoScreens/PhotosViewController.swift
@@ -0,0 +1,16 @@
+//
+//  PhotosViewController.swift
+//  TwoScreens
+//
+//  Created by Arseniy Oksenoyt on 06.07.2022.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
